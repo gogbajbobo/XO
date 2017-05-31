e06390cd78d1d1ea51cab4326aa162eeabff20bf
gogbajbobo <grigoriev.maxim@gmail.com>
2017-05-31 11:29:45 +0300

    format

--- XO/Classes/STXOGamePlay.m
+++ XO/Classes/STXOGamePlay.m
@@ -25,10 +25,13 @@
     STXOGamePlay *gamePlay = [[STXOGamePlay alloc] init];
     
     int rnd = arc4random() % 2;
-    gamePlay.playerO = [players objectAtIndex:rnd];
-    gamePlay.playerX = [players objectAtIndex:1-rnd];
+    gamePlay.playerO = players[rnd];
+    gamePlay.playerX = players[1-rnd];
+    
     gamePlay.currentPlayer = gamePlay.playerX;
-    gamePlay.field = [STXOField initWithHCount:3 VCount:3];
+    
+    gamePlay.field = [STXOField initWithHCount:3
+                                        VCount:3];
     
     for (STXOPlayer *player in players) {
         player.field = gamePlay.field;
@@ -48,11 +51,13 @@
     NSArray *players = [NSArray arrayWithObjects:you, opp, nil];
     
     int rnd = arc4random() % 2;
-    gamePlay.playerO = [players objectAtIndex:rnd];
-    gamePlay.playerX = [players objectAtIndex:1-rnd];
-//    gamePlay.currentPlayer = gamePlay.playerX;
+    gamePlay.playerO = players[rnd];
+    gamePlay.playerX = players[1-rnd];
+
     gamePlay.currentPlayer = gamePlay.you;
-    gamePlay.field = [STXOField initWithHCount:3 VCount:3];
+    
+    gamePlay.field = [STXOField initWithHCount:3
+                                        VCount:3];
     
     for (STXOPlayer *player in players) {
         player.field = gamePlay.field;
@@ -64,26 +69,39 @@
 
 
 - (void)setPlayerX:(STXOPlayer *)player {
+    
     player.gamePic = @"X";
     player.gameValue = 1;
     _playerX = player;
+    
 }
 
 - (void)setPlayerO:(STXOPlayer *)player {
+    
     player.gamePic = @"O";
     player.gameValue = -1;
     _playerO = player;
+    
 }
 
 - (void)setCurrentPlayer:(STXOPlayer *)currentPlayer {
     
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    
     if (currentPlayer == self.you) {
-        NSLog(@"%@", self);
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"yourMove" object:self];
+        
+        NSLog(@"you are current player %@", self);
+        [nc postNotificationName:@"yourMove"
+                          object:self];
+        
     } else if (currentPlayer == self.opp) {
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"oppsMove" object:self];
+        
+        [nc postNotificationName:@"oppsMove"
+                          object:self];
+        
     }
     _currentPlayer = currentPlayer;
+    
 }
 
 - (void)setYou:(STXOPlayer *)you {
@@ -104,42 +122,45 @@
 
 - (BOOL)move:(NSString *)move toH:(NSInteger)h andV:(NSInteger)v {
     
-//    if ([self.field move:move toH:h V:v]) {
+    BOOL GP = [self.field setValue:self.currentPlayer.gameValue
+                               toH:h
+                              andV:v];
     
-    BOOL GP = [self.field setValue:self.currentPlayer.gameValue toH:h andV:v];
+    NSLog(@"GP %d", GP);
     
-//    NSLog(@"GP %d", GP);
-    
-    if (GP) {
-
-        STXOCell lastMove;
-        lastMove.h = h;
-        lastMove.v = v;
-        lastMove.gamePic = (char)[self.currentPlayer.gamePic UTF8String];
-        self.lastMove = lastMove;
-        
-        NSValue *move = [NSValue valueWithBytes:&lastMove objCType:@encode(STXOCell)];
-        [self.field.cells[h] replaceObjectAtIndex:v withObject:move];
-        
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"lastMove" object:self userInfo:[NSDictionary dictionaryWithObject:move forKey:@"move"]];
-
-        self.currentPlayer = (self.currentPlayer == self.playerX) ? self.playerO : self.playerX;
-        
-        BOOL CM = [self checkMove];
-        
-//        NSLog(@"CM %d", CM);
-        
-        return YES;
-
-        if (CM) {
-            
-            return YES;
-
-        }
-        
+    if (!GP) {
+        return NO;
     }
+
+    STXOCell lastMove;
+    lastMove.h = h;
+    lastMove.v = v;
+    lastMove.gamePic = (char)self.currentPlayer.gamePic.UTF8String;
+    self.lastMove = lastMove;
     
-    return NO;
+    NSValue *gameMove = [NSValue valueWithBytes:&lastMove
+                                       objCType:@encode(STXOCell)];
+    
+    [self.field.cells[h] replaceObjectAtIndex:v
+                                   withObject:gameMove];
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"lastMove"
+                                                        object:self
+                                                      userInfo:@{@"move": gameMove}];
+    
+    self.currentPlayer = (self.currentPlayer == self.playerX) ? self.playerO : self.playerX;
+    
+    BOOL CM = [self checkMove];
+    
+    NSLog(@"CM %@", @(CM));
+    
+    return CM;
+    
+//    if (CM) {
+//        
+//        return YES;
+//        
+//    }
     
 }
 
@@ -149,7 +170,7 @@
     BOOL CV = [self checkVertical];
     BOOL CD = [self checkDiagonal];
     
-//    NSLog(@"CH%d, CV%d, CD%d", CH, CV, CD);
+    NSLog(@"CH%@, CV%@, CD%@", @(CH), @(CV), @(CD));
     
     return (CH || CV || CD);
     
@@ -161,7 +182,7 @@
     BOOL result = YES;
     STXOCell cell;
     
-    for (int i = 0; i < self.field.vcount; i++) {
+    for (NSInteger i = 0; i < self.field.vcount; i++) {
         
         [self.field.cells[self.lastMove.h][i] getValue:&cell];
         result = result && (cell.gamePic == gamePic);
@@ -182,7 +203,7 @@
     BOOL result = YES;
     STXOCell cell;
 
-    for (int i = 0; i < self.field.hcount; i++) {
+    for (NSInteger i = 0; i < self.field.hcount; i++) {
 
         [self.field.cells[i][self.lastMove.v] getValue:&cell];
         result = result && (cell.gamePic == gamePic);
@@ -199,81 +220,75 @@
 
 - (BOOL)checkDiagonal {
     
-    int h = self.lastMove.h - 1;
-    int v = self.lastMove.v - 1;
+    NSInteger h = self.lastMove.h - 1;
+    NSInteger v = self.lastMove.v - 1;
     char gamePic = self.lastMove.gamePic;
 
-    if (((abs(h) == 1) && v == 0) || ((abs(v) == 1) && h == 0)) {
-        
+    if (((labs(h) == 1) && v == 0) || ((labs(v) == 1) && h == 0)) {
         return NO;
+    }
+    
+    if (v == 0 && h == 0) {
         
-    } else {
+        BOOL result1 = YES;
+        BOOL result2 = YES;
+        STXOCell cell;
         
-        if (v == 0 && h == 0) {
+        for (int i = 0; i < self.field.hcount; i++) {
             
-            BOOL result1 = YES;
-            BOOL result2 = YES;
-            STXOCell cell;
-
-            for (int i = 0; i < self.field.hcount; i++) {
-
-                [self.field.cells[i][i] getValue:&cell];
-                result1 = result1 && (cell.gamePic == gamePic);
-                
-                [self.field.cells[i][self.field.hcount-1-i] getValue:&cell];
-                result2 = result2 && (cell.gamePic == gamePic);
-
-            }
+            [self.field.cells[i][i] getValue:&cell];
+            result1 = result1 && (cell.gamePic == gamePic);
             
-            if (result1) {
-                NSLog(@"+diagonal");
-            }
-            if (result2) {
-                NSLog(@"-diagonal");
-            }
-            
-            return result1 || result1;
-            
-        } else {
-            
-            BOOL result = YES;
-            STXOCell cell;
-            
-            if (h * v == 1) {
-
-                for (int i = 0; i < self.field.hcount; i++) {
-                    
-                    [self.field.cells[i][i] getValue:&cell];
-                    result = result && (cell.gamePic == gamePic);
-
-                }
-                
-                if (result) {
-                    NSLog(@"+diagonal");
-                }
-                
-            } else if (h * v == -1) {
-
-                for (int i = 0; i < self.field.hcount; i++) {
-                    
-                    [self.field.cells[i][self.field.hcount-1-i] getValue:&cell];
-                    result = result && (cell.gamePic == gamePic);
-                    
-                }
-                
-                if (result) {
-                    NSLog(@"-diagonal");
-                }
-
-            }
-            
-            return result;
+            [self.field.cells[i][self.field.hcount-1-i] getValue:&cell];
+            result2 = result2 && (cell.gamePic == gamePic);
             
         }
         
+        if (result1) {
+            NSLog(@"+diagonal");
+        }
+        if (result2) {
+            NSLog(@"-diagonal");
+        }
+        
+        return result1 || result1;
+        
+    } else {
+        
+        BOOL result = YES;
+        STXOCell cell;
+        
+        if (h * v == 1) {
+            
+            for (int i = 0; i < self.field.hcount; i++) {
+                
+                [self.field.cells[i][i] getValue:&cell];
+                result = result && (cell.gamePic == gamePic);
+                
+            }
+            
+            if (result) {
+                NSLog(@"+diagonal");
+            }
+            
+        } else if (h * v == -1) {
+            
+            for (int i = 0; i < self.field.hcount; i++) {
+                
+                [self.field.cells[i][self.field.hcount-1-i] getValue:&cell];
+                result = result && (cell.gamePic == gamePic);
+                
+            }
+            
+            if (result) {
+                NSLog(@"-diagonal");
+            }
+            
+        }
+        
+        return result;
+        
     }
-    
-    return NO;
     
 }
 
